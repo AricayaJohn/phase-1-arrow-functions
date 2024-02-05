ab70a25032c4e9b00994d2432f441a02d42aaf4f
John Aricaya <jaricaya@gmail.com>
2024-02-04 21:55:06 -0800

    Arrow Functions lab completed

---
+++ index.js
@@ -0,0 +1,17 @@
+let a = 2000;
+let b = 100;
+
+const divide = (a, b) => a / b;
+// has a function expression called divide
+// divide divides 2000 by 100
+
+// has an arrow function called square
+// square arrow function takes one parameter and multiplies it times itself
+let x = 2 
+const square = (x) => x ** x;
+
+
+// has an arrow function called add
+// add arrow function takes two parameters and adds them together
+const add = (a, b) => a + b;
+
